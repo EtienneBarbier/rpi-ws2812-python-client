080feac64d0dd8ca0291dbc1eae9e72257136959
Etienne Barbier <etienne.jeux@gmail.com>
2018-10-02 09:51:29 +0200

    Color remember

--- src/app/wheel-color-picker/wheel-color-picker.component.ts
+++ src/app/wheel-color-picker/wheel-color-picker.component.ts
@@ -19,13 +19,23 @@ export class WheelColorPickerComponent implements OnInit {
   constructor(private httpClient: HttpClient) {
     first_request = true;
     demoColorPicker = new iro.ColorPicker("#color-picker-container", {
-      color: {r: 255, g: 0, b: 0},
+      color: {r: 255, g: 255, b: 255},
       borderWidth: 0,
       sliderMargin: 24,
       sliderHeight: 24,
       removeSlider: true,
     });
-    demoColorPicker.on("mount", function() {
+    demoColorPicker.on("mount", () => {
+      var color: any = [255,255,255];
+      this.httpClient.get('http://127.0.0.1:5000/settings').subscribe(
+        (result) => {
+          color = result["color"];
+          demoColorPicker.color.rgb =  { r: color[0], g: color[1], b: color[2] };
+        },
+        (error) => {
+          console.log('Erreur ! : ' + error);
+        }
+      );
       $(".iro__marker__outer").remove();
       // $(".iro__slider").attr("style","display: none; visibility: hidden; pointer-events: none;");
       $(".iro__marker__inner").attr("fill",'white');
@@ -35,31 +45,12 @@ export class WheelColorPickerComponent implements OnInit {
     });
   }
 
-  // var demoColorPicker = new iro.ColorPicker("#color-picker-container");
-
   ngOnInit() {
 
   }
 
 
   ngAfterViewInit(){
-    // demoColorPicker.on("color:change", function(color, changes, that=this) {
-    //   // Log the color's hex RGB value to the dev console
-    //   console.log(color.hexString);
-    //   console.log(this);
-    //   // that.httpClient.get('http://127.0.0.1:5000/color?red=255').subscribe(
-    //   //   () => {
-    //   //   },
-    //   //   (error) => {
-    //   //     console.log('Erreur ! : ' + error);
-    //   //   }
-    //   // );
-    //   // If the "H" channel has changed, log the color's HSV value too
-    //   if (changes.h) {
-    //     console.log(color.hsv);
-    //   }
-    // });
-
     demoColorPicker.on("color:change", (color, changes) => {
       if(first_request){
         first_request = false;
@@ -67,8 +58,6 @@ export class WheelColorPickerComponent implements OnInit {
       }
 
       // Log the color's hex RGB value to the dev console
-      console.log(color.hexString);
-      console.log(color.rgb);
       this.httpClient.get('http://127.0.0.1:5000/color?red='+color.rgb.r+'&blue='+color.rgb.b+'&green='+color.rgb.g).subscribe(
         () => {
         },
@@ -76,13 +65,6 @@ export class WheelColorPickerComponent implements OnInit {
           console.log('Erreur ! : ' + error);
         }
       );
-      // If the "H" channel has changed, log the color's HSV value too
-      if (changes.h) {
-        console.log(color.hsv);
-      }
     });
-
   }
-
-
 }
