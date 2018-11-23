e8898b9d94049ca2876fe4c7fd1e70e5276beca1
Etienne Barbier <barbier.dev@gmail.com>
2018-11-23 17:51:36 +0100

    Get annimation list directly from api

--- src/app/animation-list/animation-list.component.ts
+++ src/app/animation-list/animation-list.component.ts
@@ -6,8 +6,9 @@ import { AppConfigService } from '../app-config.service'
 
 
 export interface Animation {
-  value: string;
-  viewValue: string;
+  id: string;
+  name: string;
+  descrition: string;
 }
 
 @Component({
@@ -17,22 +18,31 @@ export interface Animation {
 })
 export class AnimationListComponent implements OnInit {
 
-  animations: Animation[] = [
-    {value: 'theater_chase_rainbow', viewValue: 'Theater Chase Rainbow'},
-    {value: 'rainbow', viewValue: 'Rainbow'},
-    {value: 'rainbow_cycle', viewValue: 'Rainbow Cycle'},
-    {value: 'rainbow_random', viewValue: 'Rainbow Random'},
-    {value: 'all', viewValue: 'All'},
-  ];
+  annimations: Animation[] = null;
+  // animations: Animation[] = [
+  //   {value: 'theater_chase_rainbow', viewValue: 'Theater Chase Rainbow'},
+  //   {value: 'rainbow', viewValue: 'Rainbow'},
+  //   {value: 'rainbow_cycle', viewValue: 'Rainbow Cycle'},
+  //   {value: 'rainbow_random', viewValue: 'Rainbow Random'},
+  //   {value: 'all', viewValue: 'All'},
+  // ];
   constructor(private httpClient: HttpClient, public appConfig: AppConfigService) { }
 
   ngOnInit() {
+    this.httpClient.get(this.appConfig.getConfig().apiUrl+'/annimation').subscribe(
+      (data) => {
+        this.animations = data;
+      },
+      (error) => {
+        console.log('Erreur ! : ' + error);
+      }
+    );
   }
 
   OnPlayButtonClick(event,animation){
     console.log(animation);
 
-    this.httpClient.post(this.appConfig.getConfig().apiUrl+'/annimation', { id :animation.value }).subscribe(
+    this.httpClient.post(this.appConfig.getConfig().apiUrl+'/annimation', { id :animation.id }).subscribe(
       () => {
 
       },
